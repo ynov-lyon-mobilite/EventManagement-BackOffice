6e8bbbd28746e050a5a75116aad4ff0291479dab
Jérémy Gendre <jgendre@media-sante.com>
2021-10-13 17:01:57 +0200

    add login page design and logic

---
+++ hooks/useLog.ts
@@ -0,0 +1,7 @@
+import {useEffect} from "react";
+
+export function useLog(state){
+    useEffect(() => {
+        console.log(state);
+    }, [state])
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 import UserContextProvider from "../context/UserContext";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            // Purple and green play nicely together.
+            main: '#16887F',
+        },
+        secondary: {
+            // This is green.A700 as hex.
+            main: '#42AB9E',
+            contrastText: '#fff',
+        },
+    },
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-      <UserContextProvider>
-          <Component {...pageProps} />
-      </UserContextProvider>
+      <ThemeProvider theme={theme}>
+          <UserContextProvider>
+              <Component {...pageProps} />
+          </UserContextProvider>
+      </ThemeProvider>
   );
 }
 
