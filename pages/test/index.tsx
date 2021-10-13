ca60261686f2037542bfeb99150515ea3efe0305
Jérémy Gendre <jgendre@media-sante.com>
2021-10-13 10:38:16 +0200

    setup project

---
+++ pages/test/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Test() {
+    return (
+        <div>
+            test
+            <Link href="/test/test-page"><a>test-page</a></Link>
+        </div>
+    );
+}
