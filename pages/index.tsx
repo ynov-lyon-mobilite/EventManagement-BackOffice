aab446e45fc4d10f10c08c4cad1998d85eeb79d6
Jérémy Gendre <jgendre@media-sante.com>
2021-10-13 14:37:20 +0200

    login logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import Layout from "../components/layout/Layout";
+import {useContext} from "react";
+import {UserContext} from "../context/UserContext";
+import Login from "./login";
 
 export default function Home() {
-  return (
-    <Layout>
-       welcome
-    </Layout>
-  )
+    const {user} = useContext(UserContext);
+
+    if(!user){
+        return (<Login/>);
+    }
+
+    return (
+        <Layout>
+           welcome
+        </Layout>
+    )
 }
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,9 @@
+import {useContext} from "react";
+import {UserContext} from "../../context/UserContext";
+
 export default function Login(){
+    const {setUser} = useContext(UserContext);
+
     return (
         <div>
             login
