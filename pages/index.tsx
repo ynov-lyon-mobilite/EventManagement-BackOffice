62cce9368b9e3c31beee876ecbcb19b638de701a
Jérémy Gendre <jgendre@media-sante.com>
2021-11-24 16:16:42 +0100

    add buttons in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Layout from "../components/layout/Layout";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
-import { useRouter } from "next/router";
 import Unauthorized from "../components/error/403";
-import Link from "next/link";
 import {NextSeo} from "next-seo";
+import {Fab} from "@mui/material";
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import EventIcon from '@mui/icons-material/Event';
+import BookmarkAddIcon from '@mui/icons-material/BookmarkAdd';
+import {useRouter} from "next/router";
 
 export default function Home() {
   const { user, loading } = useContext(UserContext);
+    const router = useRouter();
 
   if (!user && !loading) {
     return <Unauthorized />;
@@ -19,7 +23,22 @@ export default function Home() {
               title="Accueil"
               description="Page d'accueil du back-office Yvent"
           />
-          Bienvenue
+          <div className="d-flex">
+              <div className="m-auto d-flex justify-around w-full flex-wrap">
+                  <Fab variant="extended" className="m-2 font-bold" onClick={() => {router.push('/profile')}}>
+                      <ManageAccountsIcon sx={{ mr: 1 }} />
+                      Mon profil
+                  </Fab>
+                  <Fab variant="extended" className="m-2 font-bold" onClick={() => {router.push('/events')}}>
+                      <EventIcon sx={{ mr: 1 }} />
+                      Evènements
+                  </Fab>
+                  <Fab variant="extended" className="m-2 font-bold" onClick={() => {router.push('/bookings')}}>
+                      <BookmarkAddIcon sx={{ mr: 1 }} />
+                      Réservations
+                  </Fab>
+              </div>
+          </div>
       </Layout>
   );
 }
