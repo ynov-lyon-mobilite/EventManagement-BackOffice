6604f3f7e51ed8293ceccd16e2e6fe1d71c627bf
Jérémy Gendre <gendrejeremy@yahoo.fr>
2022-03-30 14:03:39 +0200

    style: change wall-paper login image and add bold text in home page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -43,59 +43,61 @@ export default function Login() {
           title="Yvent - Connexion"
           description="Page de connexion du back-office Yvent"
       />
-      <form onSubmit={handleLogin} className={styles.loginSubcontainer}>
-        <Card sx={{ minWidth: 275 }} elevation={10}>
-          <CardContent>
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              className="text-center"
-              color="primary"
-            >
-              Connexion
-            </Typography>
-            <div className="mt-2">
-              <TextField
-                required
-                autoFocus
-                type="email"
-                label="Email"
-                variant="outlined"
-                className="w-full"
-                value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-            <div className="mt-2">
-              <TextField
-                required
-                type="password"
-                label="Mot de passe"
-                variant="outlined"
-                className="w-full"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
-            {error && <div className="mt-2 error">{error}</div>}
-          </CardContent>
-          <CardActions className="d-flex justify-center">
-            <LoadingButton
-              disabled={loading}
-              loading={loading}
-              type="submit"
-              variant="contained"
-            >
-              Log in
-            </LoadingButton>
-          </CardActions>
-        </Card>
-      </form>
+      <div className={styles.loginSubcontainer}>
+        <form onSubmit={handleLogin} className="m-auto">
+          <Card sx={{ minWidth: 275 }} elevation={10}>
+            <CardContent>
+              <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="text-center"
+                  color="primary"
+              >
+                Connexion
+              </Typography>
+              <div className="mt-2">
+                <TextField
+                    required
+                    autoFocus
+                    type="email"
+                    label="Email"
+                    variant="outlined"
+                    className="w-full"
+                    value={email}
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                />
+              </div>
+              <div className="mt-2">
+                <TextField
+                    required
+                    type="password"
+                    label="Mot de passe"
+                    variant="outlined"
+                    className="w-full"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                />
+              </div>
+              {error && <div className="mt-2 error">{error}</div>}
+            </CardContent>
+            <CardActions className="d-flex justify-center">
+              <LoadingButton
+                  disabled={loading}
+                  loading={loading}
+                  type="submit"
+                  variant="contained"
+              >
+                Log in
+              </LoadingButton>
+            </CardActions>
+          </Card>
+        </form>
+      </div>
     </div>
   );
 }
